95ecce37bf4b1c853d892bb93bbf0dbb856bd024
anhthuSebela <79291005+anhthuSebela@users.noreply.github.com>
2024-07-17 18:55:32 -0400

    fixed the upperlip not fully connecting points

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ document.body.appendChild(videoElement);
 let videoTexture, backgroundPlane;
 let videoAspect;
 
-const upperLipPoints = [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 306, 291, 292, 415, 310, 311, 312, 13, 82, 81, 80, 191, 78, 62, 72, 61];
-const lowerLipPoints = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 306, 292, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95, 78, 62, 76, 61];
+const upperLipPoints = [  61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 306, 291, 292, 415, 310, 311, 312, 13, 82, 81, 80, 191, 78, 62, 76, 61];
+const lowerLipPoints = [  61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 306, 292, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95, 78, 62, 76, 61
+];
 
 function initThreeJS() {
   videoAspect = videoElement.videoWidth / videoElement.videoHeight;
